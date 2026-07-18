90722ea0e88e98ae9e24c942e3fe1d63f883d76d
Rahul Dutta <142531365+rahuldutta16@users.noreply.github.com>
2026-07-19 00:11:33 +0530

    Update scenario decorator in test_sales.py

--- tests/test_sales.py
+++ tests/test_sales.py
@@ -4,6 +4,6 @@ from qase.pytest import qase
 from steps.sales_steps import *
 
 @qase.id(1)
-@scenario("../features/sales.feature", "Verify that a saved sales order line is visible in design")
+@scenario("../features/sales.feature")
 def test_saved_sales_order():
     pass
