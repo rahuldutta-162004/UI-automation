c6ad2fd5b28a08bfb166076c62e84b796c7632da
Rahul Dutta <142531365+rahuldutta16@users.noreply.github.com>
2026-07-19 00:07:28 +0530

    Update test_sales.py

--- tests/test_sales.py
+++ tests/test_sales.py
@@ -1,5 +1,9 @@
-from pytest_bdd import scenarios
+from pytest_bdd import scenario
+from qase.pytest import qase
 
 from steps.sales_steps import *
 
-scenarios("../features/sales.feature")
+@qase.id(1)
+@scenario("../features/sales.feature", "Verify that a saved sales order line is visible in design")
+def test_saved_sales_order():
+    pass
