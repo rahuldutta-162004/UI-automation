373ea6bbca65c74fad8c01345ee99e0ea39e47d7
rahuldutta-162004 <rahul@everflourish.in>
2026-07-22 18:22:48 +0530

    Update test_mdm.py

--- tests/test_mdm.py
+++ tests/test_mdm.py
@@ -1,14 +1,5 @@
-from pytest_bdd import scenario
-from qase.pytest import qase
+from pytest_bdd import scenarios
+
 from steps.mdm_steps import *
 
-@qase.id(101)
-@scenario("../features/mdm.feature", "Edit and verify item description from MDM task")
-def test_create_customer():
-    pass
-
-
-@qase.id(102)
-@scenario("../features/mdm.feature", "Edit configuration code and validate that it can't be editable")
-def test_delete_customer():
-    pass
+scenarios("../features/mdm.feature")
