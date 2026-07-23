ece34e2916682410cdb38a74f39165ab1c55e873
rahuldutta-162004 <rahul@everflourish.in>
2026-07-23 13:32:24 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ REFRESH_TOKEN ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmV
 @pytest.fixture(scope="session")
 def authenticated_page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
 
         context = browser.new_context()
         page = context.new_page()
